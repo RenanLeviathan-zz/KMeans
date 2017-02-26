288940de82160337d38b4031109e7a57198c1024
RenanLeviathan <renan.leviathan@gmail.com>
2017-02-26 10:44:34 +0000

    #2

--- src/kmeans.py
+++ src/kmeans.py
@@ -15,8 +15,10 @@ esp=0
 cores=['Red','Blue','Yellow','Green','Gray','#50f0da']
 cnv.create_line(2,450,650,450)
 cnv.create_line(2,450,2,2)
-classes=[]
+#geraçao de centroides
 coords = kmm.gerar_centroides(3,4)
+points = []
+rnd.seed(7)
 for i in coords:
     idx = rnd.randint(0,len(cores))
     cnv.create_rectangle(i[0],i[1],i[0]+10,i[1]+10,fill=cores[idx])
@@ -25,7 +27,14 @@ for linha in arq:
     values = linha.split(sep=',')
     x=abs(float(values[0]))*100
     y=abs(float(values[1]))*100
+    points.append([x,y])
     cnv.create_oval(x+esp,y+esp,x+5+esp,y+5+esp)
+
+dists=[]
+for c in coords:
+    ds=sorted(kmm.dist_eucl(c,points))
+    dists.append(ds)
+    
 
 mainloop()
     
--- src/kmeans_module.py
+++ src/kmeans_module.py
@@ -5,12 +5,21 @@ Created on Fri Feb 24 15:09:53 2017
 @author: FACOMP
 """
 import random as rnd
+import math as mt
 def gerar_centroides(qtd,length):
     coords=[]
-    for i in range(0,qtd):
+    while qtd > 0:
         x=abs(rnd.randint(0,length))*100
         y=abs(rnd.randint(0,length))*100
         coord=[x,y]
         coords.append(coord)
+        qtd-=1
     return coords
     
+def dist_eucl(c,ps):
+    dists=[]
+    for p in ps:
+        d = mt.sqrt(mt.pow(c[0]-p[0],2)+mt.pow(c[1]-p[1],2))
+        dists.append(d)
+    return dists
+
